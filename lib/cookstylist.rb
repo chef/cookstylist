0b1ea9eca550f7298dbb59e3b2cdfeafd2928431
Tim Smith <tsmith84@gmail.com>
2020-08-30 22:56:03 -0700

    Create skeleton repo class
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/cookstylist.rb
+++ lib/cookstylist.rb
@@ -2,6 +2,7 @@ module Cookstylist
   require_relative "cookstylist/log"
   require_relative "cookstylist/github"
   require_relative "cookstylist/installation"
+  require_relative "cookstylist/repo"
 
   def self::run
     installations = Cookstylist::Github.instance.installation_ids
---
+++ lib/cookstylist/repo.rb
@@ -0,0 +1,20 @@
+module Cookstylist
+  class Repo
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def clone
+
+    end
+
+    def cookstyle_branch_exists?
+
+    end
+
+    def checkout_cookstyle_branch
+
+    end
+end
