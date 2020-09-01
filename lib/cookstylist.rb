66ed66b548365f4a6a816a12315e2f1da9d54983
Tim Smith <tsmith84@gmail.com>
2020-08-31 19:51:00 -0700

    Print out some summary data
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/cookstylist.rb
+++ lib/cookstylist.rb
@@ -16,8 +16,24 @@ module Cookstylist
         puts "#{repo_data[:full_name]}:"
         puts "  looks_like_cookbook?: #{repo.looks_like_cookbook?}"
         puts "  fork?: #{repo.fork?}"
-        next unless !repo.fork? and repo.looks_like_cookbook?
+        next unless !repo.fork? && repo.looks_like_cookbook?
+
         puts "  Cloned to #{repo.clone}"
+
+        print "  Running Cookstyle against the local repo: "
+        corrector = Cookstylist::Corrector.new(repo.local_path)
+        corrector.run
+        puts "#{corrector.summary["offense_count"]} offenses!"
+
+        # print a summary
+        results = corrector.results_by_cop
+        results.each do |cop, offenses|
+          files = offenses.filter_map { |x| x["file_path"] if x["corrected"] }
+          next if files.empty?
+
+          puts cop
+          files.each { |x| puts "  - #{x}" }
+        end
       end
     end
   end
