91b0abcc09575997480a6443926d547a5bd6faf2
Tim Smith <tsmith84@gmail.com>
2020-08-31 19:50:37 -0700

    add results_by_cop to corrector
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/cookstylist/corrector.rb
+++ lib/cookstylist/corrector.rb
@@ -31,6 +31,32 @@ module Cookstylist
     def summary
       @results["summary"]
     end
+
+    #
+    # RuboCop json output is by file, which isn't super handy when writing out summary reports
+    # this method flips that output to be by cop, which is a bit easier to quickly scan through
+    # and if there's a good number of RuboCop style violations it's easier to ignore those and
+    # focus just on the ChefWhatever departments
+    #
+    # @return [Hash] Hash of cops names with arrays of offense hashes
+    #
+    def results_by_cop
+      cop_results = {}
+
+      @results["files"].each do |file|
+        next if file["offenses"].empty? # we don't care about files w/o offenses
+
+        file["offenses"].each do |offense|
+          # create the array for this particular cop if it's not already there
+          cop_results[offense["cop_name"]] = [] unless cop_results[offense["cop_name"]]
+
+          # Add the file path to the offense since we're flipping the data structure
+          # make sure we don't expose where we store the files though
+          offense["file_path"] = file["path"].delete_prefix(@path)
+          cop_results[offense["cop_name"]] << offense
+        end
+      end
+      cop_results
     end
   end
 end
