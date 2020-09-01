72a1ff18908326a25197c5bea1407a2c3d35f095
Tim Smith <tsmith84@gmail.com>
2020-08-31 20:10:52 -0700

    Fixup repo to checkout branches and know status
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/cookstylist/repo.rb
+++ lib/cookstylist/repo.rb
@@ -3,13 +3,15 @@ module Cookstylist
     require "git"
     require "fileutils"
 
-    attr_reader :name, :local_path
+    attr_reader :name, :local_path, :git_repo
 
     def initialize(metadata)
       @name = metadata[:full_name]
       @metadata = metadata
       @local_path = ::File.join(Dir.tmpdir, @name.gsub(/[^0-9A-Z]/i, "_"))
+      @git_repo = nil
       @gh_conn = Cookstylist::Github.instance.connection
+      require 'pry'; binding.pry
     end
 
     #
@@ -45,7 +47,7 @@ module Cookstylist
       # delete any existing checked out repos
       FileUtils.rm_rf(@local_path)
 
-      Git.clone(uri, local_dir, { path: Dir.tmpdir })
+      @git_repo = Git.clone(uri, local_dir, { path: Dir.tmpdir })
 
       @local_path
     end
@@ -54,11 +56,23 @@ module Cookstylist
     # @return [Boolean] Does a branch named 'cookstyle' already exist?
     #
     def cookstyle_branch_exists?
-      @gh_conn.branches(@name).any? { |x| x[:name] == "cookstyle" }
+      !!@git_repo.branches[:cookstyle]
     end
 
+    #
+    # Checkout (and create if necessary) the cookstyle branc
+    #
+    # @return [void]
+    #
     def checkout_cookstyle_branch
+      @git_repo.branch('cookstyle').checkout
+    end
 
+    #
+    # @return [Boolean] Are there uncommitted changes in the repo
+    #
+    def dirty?
+      !@git_repo.status.changed.empty?
     end
   end
 end
