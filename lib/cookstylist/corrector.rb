8b79189f22d617d0c8626e682a8268ab1036156f
Tim Smith <tsmith84@gmail.com>
2020-09-11 21:27:33 -0700

    Skip the name cop since the autocorrect is totally wrong here
    Since we checkout to a funky tmp dir it's all wrong
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/cookstylist/corrector.rb
+++ lib/cookstylist/corrector.rb
@@ -22,12 +22,12 @@ module Cookstylist
     # @return [String] What is the repo's default branch
     #
     def run
-      cmd = Mixlib::ShellOut.new("cookstyle --format json -a #{@path}")
+      cmd = Mixlib::ShellOut.new("cookstyle --format json -a --except ChefCorrectness/MetadataMissingName #{@path}")
       cmd.run_command
 
       # rubocop will error out on old configs a lot so ignore the config if we fail
       if cmd.error?
-        cmd = Mixlib::ShellOut.new("cookstyle --format json --force-default-config -a #{@path}")
+        cmd = Mixlib::ShellOut.new("cookstyle --format json --force-default-config -a --except ChefCorrectness/MetadataMissingName #{@path}")
         cmd.run_command
       end
 
